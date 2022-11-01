201dc2d0e6db385604526541b0dbc72caf4c276c
bobTheBuilderCodes <91603698+bobTheBuilderCodes@users.noreply.github.com>
2022-11-01 16:40:55 +0000

    Update schoolRegistrationSlice

--- frontend/src/features/auth/schoolRegistrationSlice.js
+++ frontend/src/features/auth/schoolRegistrationSlice.js
@@ -4,14 +4,17 @@ export const registerSchool = createAsyncThunk(
   "signUp/registerSchool",
   async (args, { rejectWithValue }) => {
     try {
-      const response = await fetch("http://localhost:5000/api/v1/signup", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(args),
-      });
+      const response = await fetch(
+        "http://localhost:5000/api/v1/createSchool",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(args),
+        }
+      );
 
       const data = await response.json();
 
