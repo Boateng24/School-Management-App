f559934909dbb71c29eeec251185483bfe687f09
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2022-11-12 14:27:38 +0000

    Student profile Page

--- frontend/src/components/students/StudentDetails.jsx
+++ frontend/src/components/students/StudentDetails.jsx
@@ -1,9 +1,62 @@
 import { Divider } from "@mui/material";
 import React from "react";
 import { useParams } from "react-router-dom";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { useTheme } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import StudentClasses from "./StudentClasses";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ py: 1 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
 
 const StudentDetails = () => {
   const { studentId } = useParams();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   console.log(studentId);
   return (
     <div className="flex">
@@ -41,7 +94,43 @@ const StudentDetails = () => {
         </div>
         {/* <img src="" alt="Student Profiles" /> */}
       </div>
-      <div className=" w-[80vw] mt-24 p-4 h-[74vh]">2</div>
+      <div className=" w-[80vw] mt-[53px] h-[74vh] ">
+        <Box sx={{ width: "100%" }}>
+          <AppBar
+            position="static"
+            style={{ backgroundColor: "#29365F", color: "white", margin: 0 }}
+            elevation={0}
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab label="Student Grades" {...a11yProps(0)} />
+              <Tab label="Items in Possession" {...a11yProps(1)} />
+              <Tab label="Health Information" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <SwipeableViews
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              <StudentClasses />
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              Items in possession
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              Health Information
+            </TabPanel>
+          </SwipeableViews>
+        </Box>
+      </div>
     </div>
   );
 };
