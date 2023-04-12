51941db3d7416bfbf3c8e4fe38cb1441f751b14a
tuffourboateng-amalitech <99021465+tuffourboateng-amalitech@users.noreply.github.com>
2023-04-12 10:23:30 +0000

    superadmin delete file now restored

--- backend/src/controllers/superAdmin.controller.ts
+++ backend/src/controllers/superAdmin.controller.ts
@@ -4,7 +4,6 @@ import { compare } from '../helpers/bcryptConfig';
 import { createAccessToken } from '../helpers/accessToken';
 import { createRefreshToken } from '../helpers/refreshToken';
 
-
 const maxAge = 7 * 24 * 60 * 60 * 1000;
 export const superAdminLogin = async (
   req: Request,
@@ -21,32 +20,32 @@ export const superAdminLogin = async (
     if (!findUser) {
       return res.status(404).json({ message: 'User does not exist' });
     }
-    
+
     const matchPassword = await compare(password, findUser?.password);
     if (!matchPassword) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
 
     // create both acess and refresh token for current user
-const accessToken = await createAccessToken(findUser?.id);
-const refreshToken = await createRefreshToken(findUser?.id);
+    const accessToken = await createAccessToken(findUser?.id);
+    const refreshToken = await createRefreshToken(findUser?.id);
 
-res.cookie('jwt-access', refreshToken, {
-  httpOnly: true,
-  sameSite: 'none',
-  secure: true,
-  maxAge,
-});
+    res.cookie('jwt-access', refreshToken, {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+      maxAge,
+    });
 
-const loggedInUser = {
-  id: findUser.id,
-  firstname: findUser.fullname,
-  email: findUser.email,
-  role: findUser.role,
-  accessToken,
-};
+    const loggedInUser = {
+      id: findUser.id,
+      firstname: findUser.fullname,
+      email: findUser.email,
+      role: findUser.role,
+      accessToken,
+    };
 
-res.status(200).json({ loggedInUser, success: true });
+    res.status(200).json({ loggedInUser, success: true });
   } catch (error) {
     next(error);
   }
